da754b2f105a6c7e87cc694946b3f0456844fc4c
Moritz von Göwels <moritz@tarn-vedra.de>
2025-01-07 10:02:04 +0100

    schema: Add index on `components.component`

--- src/schema.sql
+++ src/schema.sql
@@ -1,8 +1,9 @@
 create table if not exists components (
-	entity integer not null,
-	component text not null,
-	data blob
+    entity integer not null,
+    component text not null,
+    data blob
 );
 
-create unique index if not exists components_entity_component_unqiue 
-	on components (entity, component);
+create unique index if not exists components_entity_component_unqiue_idx on components (entity, component);
+
+create index if not exists components_component_idx on components (component);
