e8abcb6c9233affad87c23bf6e957517ec8049b0
Moritz von Göwels <moritz@tarn-vedra.de>
2025-05-21 10:15:32 +0200

    ecsdb_derive: Fix tests

--- ecsdb_derive/tests/derive_test.rs
+++ ecsdb_derive/tests/derive_test.rs
@@ -1,13 +1,18 @@
+// Shims
 pub mod component {
     pub struct JsonStorage;
     pub struct BlobStorage;
     pub struct NullStorage;
     pub trait Component {
         type Storage;
+        const NAME: &'static str;
 
-        fn component_name() -> &'static str;
+        fn component_name() -> &'static str {
+            Self::NAME
+        }
     }
 }
+
 pub mod resource {
     pub trait Resource: super::component::Component {
         fn resource_name() -> &'static str {
@@ -16,38 +21,48 @@ pub mod resource {
     }
 }
 
+// Necessary for development as we derive `ecsdb::Component for ...`
+use crate as ecsdb;
+use ecsdb::component::Component;
+use ecsdb::resource::Resource;
+use ecsdb_derive::{Component, Resource};
+
 #[test]
 fn test_component() {
-    // Necessary for development as we derive `ecsdb::Component for ...`
-    use crate as ecsdb;
-    use ecsdb::component::Component;
-
-    #[derive(ecsdb_derive::Component)]
+    #[derive(Component)]
     #[component(storage = "json")]
     struct Foo;
 
     assert_eq!(Foo::component_name(), "derive_test::Foo".to_string());
 
-    #[derive(ecsdb_derive::Component)]
+    #[derive(Component)]
     #[component(storage = "blob")]
     struct Foo2(pub Vec<u8>);
+
+    impl Into<Vec<u8>> for Foo2 {
+        fn into(self) -> Vec<u8> {
+            self.0
+        }
+    }
+
+    impl From<Vec<u8>> for Foo2 {
+        fn from(value: Vec<u8>) -> Self {
+            Self(value)
+        }
+    }
 
     assert_eq!(Foo2::component_name(), "derive_test::Foo2".to_string());
 
     let _: Vec<u8> = Foo2(vec![]).into();
     let _ = Foo2::from(vec![]);
 
-    #[derive(ecsdb_derive::Component)]
+    #[derive(Component)]
     struct Unit;
 }
 
 #[test]
 fn test_resource() {
-    // Necessary for development as we derive `ecsdb::Component for ...`
-    use crate as ecsdb;
-    use ecsdb::resource::Resource;
-
-    #[derive(ecsdb_derive::Resource)]
+    #[derive(Resource)]
     struct Foo;
 
     assert_eq!(Foo::resource_name(), "derive_test::Foo".to_string());
