36ebb98cd71d4203550d369e6b5f03a90835eb02
Moritz von Göwels <moritz@tarn-vedra.de>
2025-09-01 21:47:47 +0200

    schedule::{After,Once,...}

--- src/schedule.rs
+++ src/schedule.rs
@@ -1,15 +1,15 @@
-use crate::{system, Entity};
+use crate::{system, IntoSystem, LastRun, System};
 
-pub trait SchedulingMode: std::fmt::Debug {
-    fn should_run(&self, ecs: &crate::Ecs, system: Entity) -> bool;
-    fn did_run(&self, _ecs: &crate::Ecs, _system: Entity) {}
+pub trait SchedulingMode: std::fmt::Debug + 'static {
+    fn should_run(&self, ecs: &crate::Ecs, system: &str) -> bool;
+    fn did_run(&self, _ecs: &crate::Ecs, _system: &str) {}
 }
 
 #[derive(Debug)]
 pub struct Manually;
 
 impl SchedulingMode for Manually {
-    fn should_run(&self, _ecs: &crate::Ecs, _system: Entity) -> bool {
+    fn should_run(&self, _ecs: &crate::Ecs, _system: &str) -> bool {
         false
     }
 }
@@ -18,7 +18,7 @@ impl SchedulingMode for Manually {
 pub struct Always;
 
 impl SchedulingMode for Always {
-    fn should_run(&self, _ecs: &crate::Ecs, _system: Entity) -> bool {
+    fn should_run(&self, _ecs: &crate::Ecs, _system: &str) -> bool {
         true
     }
 }
@@ -27,19 +27,49 @@ impl SchedulingMode for Always {
 pub struct Every(pub chrono::Duration);
 
 impl SchedulingMode for Every {
-    fn should_run(&self, _ecs: &crate::Ecs, system: Entity) -> bool {
-        system
-            .component::<system::LastRun>()
+    fn should_run(&self, ecs: &crate::Ecs, system: &str) -> bool {
+        ecs.system_entity(system)
+            .and_then(|e| e.component::<system::LastRun>())
             .map(|last_run| chrono::Utc::now().signed_duration_since(&last_run.0) > self.0)
             .unwrap_or(true)
     }
 }
 
 #[derive(Debug)]
-pub struct Oneshot;
+pub struct Once;
 
-impl SchedulingMode for Oneshot {
-    fn should_run(&self, _ecs: &crate::Ecs, system: Entity) -> bool {
-        system.component::<system::LastRun>().is_none()
+impl SchedulingMode for Once {
+    fn should_run(&self, ecs: &crate::Ecs, system: &str) -> bool {
+        let entity = ecs.get_or_create_system_entity(system);
+        entity.component::<system::LastRun>().is_none()
+    }
+}
+
+#[derive(Debug)]
+pub struct After(String);
+
+impl After {
+    pub fn system<Marker, S>(system: S) -> Self
+    where
+        S: IntoSystem<Marker>,
+    {
+        Self(system.into_system().name().into())
+    }
+}
+
+impl SchedulingMode for After {
+    fn should_run(&self, ecs: &crate::Ecs, system: &str) -> bool {
+        let predecessor_last_run = ecs.system_entity(&self.0).and_then(|e| e.component());
+
+        let our_last_run = ecs
+            .system_entity(system)
+            .and_then(|e| e.component::<LastRun>());
+
+        match (predecessor_last_run, our_last_run) {
+            (None, _) => false,
+            (Some(_), None) => true,
+            (Some(LastRun(before)), Some(LastRun(after))) if before > after => true,
+            (Some(_), Some(_)) => false,
+        }
     }
 }
