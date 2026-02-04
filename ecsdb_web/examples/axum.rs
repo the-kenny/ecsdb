e7f1a9d988de8e4ce9d705fede88cf1d06cc211a
Moritz von Göwels <moritz@tarn-vedra.de>
2026-02-04 12:18:33 +0100

    ecsdb_web: better component rendering

--- ecsdb_web/examples/axum.rs
+++ ecsdb_web/examples/axum.rs
@@ -1,15 +1,26 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, path::PathBuf};
 
 use axum::routing::get;
-use tracing::info;
+use tracing::{info, warn};
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     tracing_subscriber::fmt::init();
+
+    let db_file = match &std::env::args().collect::<Box<[_]>>()[..] {
+        [] | [_] => PathBuf::from("scratch/test.sqlite"),
+        [_, path] => PathBuf::from(path),
+        args => {
+            warn!(?args, "Invalid command line");
+            eprintln!("Usage: {} DBPATH", env!("CARGO_CRATE_NAME"));
+            return Ok(());
+        }
+    };
+
     use axum::Router;
 
-    let service = ecsdb_web::service("/ecsdb/", |_req: &http::Request<_>| {
-        ecsdb::Ecs::open("scratch/test.sqlite")
+    let service = ecsdb_web::service("/ecsdb/", move |_req: &http::Request<_>| {
+        ecsdb::Ecs::open(&db_file)
     });
     let service = tower::ServiceBuilder::new().service(service);
 
